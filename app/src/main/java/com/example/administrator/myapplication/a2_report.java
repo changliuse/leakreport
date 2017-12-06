f7ee99c8449efa7c9a0b81da6cd94b44a30bfdf8
changliuse <changliuse@gmail.com>
2017-12-06 13:56:49 +0200

    some changes

---
+++ app/src/main/java/com/example/administrator/myapplication/a2_report.java
@@ -0,0 +1,13 @@
+package com.example.administrator.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class a2_report extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a2__report);
+    }
+}
