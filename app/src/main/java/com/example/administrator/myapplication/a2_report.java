c1d158bfa579159bbd4b540ef128f0cdde46ed15
Sneha Das <sneha.das@aalto.fi>
2017-12-09 17:46:45 +0200

    modified popup to snackbar widget

--- app/src/main/java/com/example/administrator/myapplication/a2_report.java
+++ app/src/main/java/com/example/administrator/myapplication/a2_report.java
@@ -2,10 +2,12 @@ package com.example.administrator.myapplication;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.PopupMenu;
+
 
 public class a2_report extends AppCompatActivity {
 
@@ -18,6 +20,11 @@ public class a2_report extends AppCompatActivity {
     public void sendreportButtonClicked(View view) {
         Intent intent = new Intent(this, a2_report_respond.class);
         startActivity(intent);
+
+        Snackbar snackbar = Snackbar
+                .make(view, "Your report has been sent to KWSTF, we are looking for a plumber to fix the leaking.\\n \\nThank You!", Snackbar.LENGTH_INDEFINITE);
+
+        snackbar.show();
 
     }
 
