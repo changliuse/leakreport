ab133fd4b18ed5f0bfd76ad4d7d9b9444215dd53
changliuse <changliuse@gmail.com>
2017-12-11 01:40:35 +0200

    page b2

---
+++ app/src/main/java/com/example/administrator/myapplication/b2_register_status.java
@@ -0,0 +1,21 @@
+package com.example.administrator.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class b2_register_status extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b2_register_status);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
