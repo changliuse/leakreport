59d6a333de91f83fffb6ead5c8d1c4efed67378f
changliuse <changliuse@gmail.com>
2017-12-11 13:08:45 +0200

    logo

---
+++ app/src/main/java/com/example/administrator/myapplication/b1_register_respond.java
@@ -0,0 +1,23 @@
+package com.example.administrator.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class b1_register_respond extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b1_register_respond);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    public void openmyprofle(View view) {
+    }
+}
